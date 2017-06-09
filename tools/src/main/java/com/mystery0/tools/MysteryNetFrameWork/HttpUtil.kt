a0b188235171d7bdcbf0432626bfc7909fb8dbea
Mystery00 <mystery0dyl520@gmail.com>
2017-06-09 15:47:30 +0800

    更新build tools，出错

--- tools/src/main/java/com/mystery0/tools/MysteryNetFrameWork/HttpUtil.kt
+++ tools/src/main/java/com/mystery0/tools/MysteryNetFrameWork/HttpUtil.kt
@@ -3,6 +3,7 @@ package com.mystery0.tools.MysteryNetFrameWork
 import android.content.Context
 
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -201,7 +202,7 @@ class HttpUtil(private val context: Context)
 		}
 		else
 		{
-			val requestQueue = Volley.newRequestQueue(context)
+			val requestQueue: RequestQueue = Volley.newRequestQueue(context)
 			val stringRequest = object : StringRequest(method, this.url,
 					Response.Listener<String> { response -> responseListener!!.onResponse(1, response) },
 					Response.ErrorListener { volleyError -> responseListener!!.onResponse(0, volleyError.message) })
